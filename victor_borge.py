617847e91f7cce69e02f60e6d7872fcb3c933197
Vandan Shah <vandanshah77@gmail.com>
2018-04-10 15:48:29 +0530

    [ADD] add suppoert to check for inflated sentence

--- victor_borge.py
+++ victor_borge.py
@@ -1,8 +1,54 @@
+import ast
+def hasNumbers(inputString):
+	return any(char.isdigit() for char in inputString)
+def load_inflate():
+	values = {}
+	values['zero'] = values['none'] = values['nil'] = values['null'] = "one"
+	values['one'] = values['won'] = values['Juan'] = 'two'
+	values['two'] = values['to'] = values['too'] = values['tu'] = 'three'
+	values['three'] = 'four'
+	values['four'] = values['for'] = values['fore'] = 'five'
+	values['five'] = 'six'
+	values['six'] = 'seven'
+	values['seven'] = 'eight'
+	values['eight'] = values['ate'] = 'nine'
+	values['nine'] = 'ten'
+	values['ten'] = 'eleven'
+	values['eleven'] = 'twelve'
+	values['twelve'] = values['dozen'] = 'thirteen'
+	values['never'] = 'once'
+	values['half'] = 'one and a half'
+	values['once'] = 'twice'
+	values['twice'] = 'thrice'
+	values['single'] = 'double'
+	values['double'] = 'triple'
+	values['first'] = 'second'
+	values['second'] = 'third'
+	values['third'] = 'fourth'
+	values['fourth'] = values['forth'] = 'fifth'
+	return values
+
 flag = True
 while flag:
 	statement = raw_input("\n(Note :- If you want to 'EXIT' simply write 'E')\nEnter your statement :- ")
+	stmttoprint = ""
 	if statement == "E" or statement == "e":
 		flag = False
 	else:
-		if "won" in statement:
-			print "Matched"
+		values = load_inflate()
+		values_to_check = []
+		for key, value in values.items():
+			values_to_check.append(key)
+		for stmt in statement.split(' '):
+			if hasNumbers(stmt):
+				stmttoprint += str(ast.literal_eval(stmt) + 1) + " "
+			else:
+				flg = True
+				for chkvalue in values_to_check:
+					if chkvalue in (stmt.lower()):
+						flg = False
+						stmttoprint += stmt.lower().replace(chkvalue, values[chkvalue]) + " "
+						break
+				if flg:
+					stmttoprint += stmt + " "
+	print "Your inflated sentence is: " + stmttoprint
